670eecea20353090d7ce85413e946ddbcb478aab
Saad Mirza <saadmirza009@gmail.com>
2017-09-23 00:27:41 -0700

    Enable Touch on Hover (All Touch Screens

---
+++ index.php
@@ -0,0 +1,12 @@
+ 
+ 
+  <!-- Core  -->
+  <script src="<?= base_url() ?>assets/global/vendor/jquery/jquery.min.js"></script>
+  <script src="<?= base_url() ?>assets/global/vendor/bootstrap/bootstrap.min.js"></script>
+ <script >
+    $('.navbar').hover(
+       function(){ $(this).addClass('hover') },
+       function(){ $(this).removeClass('hover') }
+     );
+
+  </script>
